66aff91368a50046d8e318ead13e772e5b9fb33d
Ivan Bakaidov <ivan@aacidov.ru>
2021-05-03 18:57:36 +0300

    1.6.1.2 fix charset

--- LinkaWPF/Properties/AssemblyInfo.cs
+++ LinkaWPF/Properties/AssemblyInfo.cs
@@ -50,6 +50,8 @@ using System.Windows;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.1.1")]
-[assembly: AssemblyFileVersion("1.6.1.1")]
+[assembly: AssemblyCompany("LINKa")]
+[assembly: AssemblyProduct("LINKa. смотри")]
+[assembly: AssemblyVersion("1.6.1.2")]
+[assembly: AssemblyFileVersion("1.6.1.2")]
 [assembly: NeutralResourcesLanguage("ru-RU")]
